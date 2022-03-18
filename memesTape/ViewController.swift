5f555689de0919cd7a3323c04ca036fe3e8108b0
kuznet.i.sova <kis7425910@gmail.com>
2022-03-18 21:29:33 +0300

    Initial Commit

---
+++ memesTape/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  memesTape
+//
+//  Created by Ирина Кузнецова on 18.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
