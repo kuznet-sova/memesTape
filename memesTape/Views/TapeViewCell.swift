36fb5d5459a290675af25dc7d776fcd12b42b20f
kuznet.i.sova <kuznet.i.sova@gmail.com>
2022-03-25 23:12:55 +0300

    changed random selection number likes and added dictionary for correct display of plurals

--- memesTape/Views/TapeViewCell.swift
+++ memesTape/Views/TapeViewCell.swift
@@ -16,7 +16,7 @@ class TapeViewCell: UITableViewCell {
     @IBOutlet var memeDescriptionLebel: UILabel!
     
     static let reuseIdentifier = String(describing: TapeViewCell.self)
-    private var likesCount = (1...100).randomElement()
+    private var randomInt = Int.random(in: 1...100)
     private var isChosen = false
     
     override func awakeFromNib() {
@@ -31,48 +31,30 @@ class TapeViewCell: UITableViewCell {
         memeImageViev.image = UIImage(named: fullPost.memeImageName)
         memeAuthorLabel.text = fullPost.memeAuthor
         memeDescriptionLebel.text = fullPost.memeDescription
-        getLikesCount()
+        likesCounterLebel.text = likesCountUniversal(count: randomInt)
         likeButton.setImage(UIImage(systemName: "heart"), for: .normal)
         selectionStyle = .none
     }
     
-    private func declensionLikes(likeCount: Int) -> String {
-        var declensionLike: String
-//        Как доделаю основное надо не забыть вынести в отдельную функцию высчитывание значений для 2 и 3 кейса, чтобы не перечислять вручную
-        switch likeCount {
-        case 0:
-            declensionLike = ""
-        case 1, 21, 31, 41, 51, 61, 71, 81, 91:
-            declensionLike = "лайк"
-        case 2, 3, 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54, 62, 63, 64, 72, 73, 74, 82, 83, 84, 92, 93, 94:
-            declensionLike = "лайка"
-        default:
-            declensionLike = "лайков"
-        }
-        
-        return declensionLike
-    }
+    private func likesCountUniversal(count: Int) -> String {
+        let formatString : String = NSLocalizedString("likes count",
+                                                      comment: "likes count string format to be found in Localized.stringsdict")
+        let resultString : String = String.localizedStringWithFormat(formatString, count)
     
-    private func getLikesCount() {
-        guard let likes = likesCount else { return }
-        
-        let likesCountInfo = "\(likes) " + declensionLikes(likeCount: likes)
-        likesCounterLebel.text = likesCountInfo
+        return resultString;
     }
     
     private func getFullLikesInfo() {
-        if likesCount != nil {
-            if isChosen == true {
-                likeButton.setImage(UIImage(systemName: "heart"), for: .normal)
-                likesCount! -= 1
-                getLikesCount()
-                isChosen = false
-            } else {
-                likeButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
-                likesCount! += 1
-                getLikesCount()
-                isChosen = true
-            }
+        if isChosen == true {
+            likeButton.setImage(UIImage(systemName: "heart"), for: .normal)
+            randomInt -= 1
+            likesCounterLebel.text = likesCountUniversal(count: randomInt)
+            isChosen = false
+        } else {
+            likeButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+            randomInt += 1
+            likesCounterLebel.text = likesCountUniversal(count: randomInt)
+            isChosen = true
         }
     }
     
